fa98d52a60c3ad54812969a4e7717286e36e5813
Ahmed Belloula <belloulaahmed7@gmail.com>
2023-10-29 19:32:01 +0100

    fixed the successorFunction

--- Classes/RushHourPuzzle.py
+++ Classes/RushHourPuzzle.py
@@ -65,41 +65,96 @@ class RushHourPuzzle:
     
     # def successorFunction(self): so it returns an instance of the RushHourPuzzle class
     
+    # def successorFunction(self):
+    #     successors = []
+
+    #     for vehicle in self.vehicles:
+    #         id, x, y, orientation, length = vehicle
+            
+    #         successor_state = RushHourPuzzle()
+    #         successor_state.board_height = self.board_height
+    #         successor_state.board_width = self.board_width
+    #         successor_state.walls = self.walls
+    #         # Generate possible moves for the vehicle
+    #         if orientation == 'H':
+    #             for dx in [-1, 1]:
+    #                 if dx == -1 and 0 <= x + dx <= self.board_width and self.board[y][x + dx] == ' ':
+    #                     new_vehicles = [v for v in self.vehicles if v != vehicle]
+    #                     new_vehicles.append((id, x + dx, y, orientation, length))
+    #                     successor_state.vehicles = new_vehicles
+    #                     successor_state.setBoard()
+    #                     # successors.append((f"Move {id} {'Left' if dx == -1 else 'Right'}", successor_state))
+    #                     successors.append((f"Move {id} Left", successor_state))
+    #                 elif dx == 1 and 0 <= (x + length)  < self.board_width and self.board[y][x + dx + length - 1] == ' ':
+    #                     new_vehicles = [v for v in self.vehicles if v != vehicle]
+    #                     new_vehicles.append((id, x + dx, y, orientation, length))
+    #                     successor_state.vehicles = new_vehicles
+    #                     successor_state.setBoard()
+    #                     successors.append((f"Move {id} Right", successor_state))
+    #         else:
+    #             for dy in [-1, 1]:
+    #                 if dy == -1 and 0 <= y + dy <= self.board_height and self.board[y + dy][x] == ' ':
+    #                     new_vehicles = [v for v in self.vehicles if v != vehicle]
+    #                     new_vehicles.append((id, x, y + dy, orientation, length))
+    #                     successors.append((f"Move {id} {'Up' if dy == -1 else 'Down'}", successor_state))
+    #                 elif dy == 1 and 0 <= y +length  < self.board_height and self.board[y + dy + length - 1][x] == ' ':
+    #                     new_vehicles = [v for v in self.vehicles if v != vehicle]
+    #                     new_vehicles.append((id, x, y + dy, orientation, length))
+    #                     successors.append((f"Move {id} {'Up' if dy == -1 else 'Down'}", successor_state))
+
+    #     return successors
+    
     def successorFunction(self):
         successors = []
 
         for vehicle in self.vehicles:
             id, x, y, orientation, length = vehicle
             
-            successor_state = RushHourPuzzle()
-            successor_state.board_height = self.board_height
-            successor_state.board_width = self.board_width
-            successor_state.walls = self.walls
             # Generate possible moves for the vehicle
             if orientation == 'H':
                 for dx in [-1, 1]:
                     if dx == -1 and 0 <= x + dx <= self.board_width and self.board[y][x + dx] == ' ':
                         new_vehicles = [v for v in self.vehicles if v != vehicle]
                         new_vehicles.append((id, x + dx, y, orientation, length))
+                        successor_state = RushHourPuzzle()
+                        successor_state.board_height = self.board_height
+                        successor_state.board_width = self.board_width
+                        successor_state.walls = self.walls
                         successor_state.vehicles = new_vehicles
                         successor_state.setBoard()
-                        successors.append((f"Move {id} {'Left' if dx == -1 else 'Right'}", successor_state))
+                        successors.append((f"Move {id} Left", successor_state))
                     elif dx == 1 and 0 <= (x + length)  < self.board_width and self.board[y][x + dx + length - 1] == ' ':
                         new_vehicles = [v for v in self.vehicles if v != vehicle]
                         new_vehicles.append((id, x + dx, y, orientation, length))
+                        successor_state = RushHourPuzzle()
+                        successor_state.board_height = self.board_height
+                        successor_state.board_width = self.board_width
+                        successor_state.walls = self.walls
                         successor_state.vehicles = new_vehicles
                         successor_state.setBoard()
-                        successors.append((f"Move {id} {'Left' if dx == -1 else 'Right'}", successor_state))
+                        successors.append((f"Move {id} Right", successor_state))
             else:
                 for dy in [-1, 1]:
                     if dy == -1 and 0 <= y + dy <= self.board_height and self.board[y + dy][x] == ' ':
                         new_vehicles = [v for v in self.vehicles if v != vehicle]
                         new_vehicles.append((id, x, y + dy, orientation, length))
-                        successors.append((f"Move {id} {'Up' if dy == -1 else 'Down'}", successor_state))
-                    elif dy == 1 and 0 <= y +length  < self.board_height and self.board[y + dy + length - 1][x] == ' ':
+                        successor_state = RushHourPuzzle()
+                        successor_state.board_height = self.board_height
+                        successor_state.board_width = self.board_width
+                        successor_state.walls = self.walls
+                        successor_state.vehicles = new_vehicles
+                        successor_state.setBoard()
+                        successors.append((f"Move {id} Up", successor_state))
+                    elif dy == 1 and 0 <= y + length  < self.board_height and self.board[y + dy + length - 1][x] == ' ':
                         new_vehicles = [v for v in self.vehicles if v != vehicle]
                         new_vehicles.append((id, x, y + dy, orientation, length))
-                        successors.append((f"Move {id} {'Up' if dy == -1 else 'Down'}", successor_state))
+                        successor_state = RushHourPuzzle()
+                        successor_state.board_height = self.board_height
+                        successor_state.board_width = self.board_width
+                        successor_state.walls = self.walls
+                        successor_state.vehicles = new_vehicles
+                        successor_state.setBoard()
+                        successors.append((f"Move {id} Down", successor_state))
 
         return successors
-    
+
